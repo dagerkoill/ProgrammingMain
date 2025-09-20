1047b6922efc3e4b6ebe0f7548bb6197314ccef7
dagerkoill <denisdenisresetnakov@gmail.com>
2025-09-21 00:10:51 +0300

    Прошел инпут теорию(и практические с курса)

---
+++ entry/input_practice.py
@@ -0,0 +1,24 @@
+# а = input()
+# b = input()
+# c = input()
+
+# print(a)
+# print(b)
+# print(c)
+
+# d = input()
+# e = input()
+# f = input()
+
+# print(d)
+# print(e)
+# print(f)
+
+
+# a = input()
+# b = input()
+# c = input()
+
+# print(c)
+# print(b)
+# print(a)
