ff66c16f227fd12ff08906115a88c0b19ceea5df
dagerkoill <denisdenisresetnakov@gmail.com>
2025-10-03 01:19:31 +0300

    1. Сдал экзамен на сайте 2.Начал учить условные операторы if, else

---
+++ veriables/exam.py
@@ -0,0 +1,41 @@
+# a = 17 // (23 % 7) # 17 // 2 = 8  
+# b = 34 % a * 5 - 29 % 4 * 3 # 10 - 3 = 7
+
+# print(a * b) # 7 * 8 = 54
+
+
+
+# print('*****************')
+# print('*               *')
+# print('*               *')
+# print('*****************')
+
+
+
+
+# a = int(input())
+# b = int(input())
+
+# print('Квадрат суммы', a,'и', b, 'равен', (a + b)**2)
+# print('Сумма квадратов', a, 'и', b, 'равен', a ** 2 + b ** 2)
+
+
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# d = int(input())
+
+# print(a ** b + c ** d)
+
+
+
+# n = int(input())
+# nn = n * 10 + n
+# nnn = n * 100 + 10 * n + n
+# print(n + nn + nnn )
+
+
+
+
+
