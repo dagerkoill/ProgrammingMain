97f702e696ca2c9e37977d806ae346b495b03d43
dagerkoill <denisdenisresetnakov@gmail.com>
2025-10-07 21:59:57 +0300

    Выучил строки и сделал практику

---
+++ data_types/str_data_types/str_practice.py
@@ -0,0 +1,108 @@
+# print(''' "Python is a great language!", said Fred. "I don't ever remember having this much fun before." ''')
+
+
+
+
+# name = input()
+# sure_name = input()
+
+# print('Hello', name, sure_name,'!', 'You have just delved into Python')
+
+
+
+
+# name = input()
+
+# print('Футбольная команда', name, 'имеет длину', len(name), 'символов')
+
+
+
+
+# city1 = input()
+# city2 = input()
+# city3 = input()
+# a1 = len(city1)
+# a2 = len(city2)
+# a3 = len(city3)
+
+# if a1 < a2 and a1 < a3:
+#     print(city1)
+# elif a2 < a1 and a2 < a3:
+#     print(city2)
+# else:
+#     print(city3)
+# if (a1 > a2 or a1 > a2):
+#     print(city1)
+# elif a2 > a1 or a2 > a3:
+#     print(city2)
+# else:
+#     print(city3)
+
+
+
+
+# num1 = input()
+# num2 = input()
+# num3 = input()
+# a1 = len(num1)
+# a2 = len(num2)
+# a3 = len(num3)
+
+# if (2 * a2 - a1 - a3) * (2 * a3 - a2 -a1) * (2 * a1 - a2 - a3) == 0:
+#     print('YES')
+# else:
+#     print('NO')
+
+
+
+
+# text = input()
+
+# if 'синий' in text:
+#     print('YES')
+# else:
+#     print('NO')
+
+
+
+
+# text = input()
+
+# if '@' in text and '.' in text:
+#     print('YES')
+# else:
+#     print("NO")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
